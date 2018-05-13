6503b274529530177709c353eafcff1121a2e021
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-05-13 20:58:13 +1000

    more troubleshooting

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,9 @@
-//keys.js - figure out what set of credentials to return
-if(process.env.NODE_ENV === 'production') {
-  //we are in prod mode, return prod keys
-  module.exports = require('./prod');
+
+// keys.js - figure out what set of credentials to return
+if(process.env.NODE_ENV === 'production'){
+    // production environment - return prod keys
+    module.exports = require('./prod');
 } else {
-  //we are in dev, return the dev keys
-  module.exports = require('./dev');
+    // dev environment - return dev keys
+    module.exports = require('./dev');
 }
