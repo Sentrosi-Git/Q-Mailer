c4134885070bb2bc1cecf573aaeba9aacdb2defa
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-05-20 13:30:38 +1000

    created routes for logout with associated import tags

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,36 @@
 // Using import instead of require because of ES6 (webpack & babel) syntax on front end
 // uppercase naming convention because we're exporting a component
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
-//BrowserRouter expects only one child component
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-        <Header />
-        <Route exact={true} path="/" component={Landing}/>
-        <Route exact path="/surveys" component={Dashboard}/>
-        <Route path="/surveys/new" component={SurveyNew}/>
-        </div>
 
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+
+        <BrowserRouter>
+          <div>
+          <Header />
+          <Route exact={true} path="/" component={Landing}/>
+          <Route exact path="/surveys" component={Dashboard}/>
+          <Route path="/surveys/new" component={SurveyNew}/>
+          </div>
+
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+      switch (this.props.auth){
+        case null:
+          return;
+        case false:
+          return(
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+          );
+        default:
+          return <li><a href="/api/logout">Logout</a></li>;
+      }
+  }
+
   render() {
     return (
       <div>
@@ -10,9 +27,7 @@ class Header extends Component {
               Emailizer
             </a>
             <ul className="right">
-              <li>
-                <a>Login with Google</a>
-              </li>
+              {this.renderContent()}
             </ul>
           </div>
         </nav>
@@ -21,4 +36,7 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
