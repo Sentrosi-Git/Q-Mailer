9ce89b8c23ebc22a64949c7901cf4f473749d044
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-05-20 20:09:51 +1000

    added body parser middlware

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -14,7 +15,10 @@ class Header extends Component {
             </li>
           );
         default:
-          return <li><a href="/api/logout">Logout</a></li>;
+          return [
+            <li key="1"><Payments /></li>,
+            <li key="2"><a href="/api/logout">Logout</a></li>
+          ];
       }
   }
 
@@ -23,7 +27,7 @@ class Header extends Component {
       <div>
         <nav>
           <div className="nav-wrapper">
-            <Link to={this.props.auth ? '/surveys' : '/'} //ternary right here! 
+            <Link to={this.props.auth ? '/surveys' : '/'} //ternary right here!
               className="left brand-logo"
             >
               Emailizer
