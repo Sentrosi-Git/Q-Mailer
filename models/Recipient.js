3bd2a3f6f12987e1c8fd438dc2d4660cc70403f7
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-05-21 21:39:41 +1000

    survey email styling

--- models/Recipient.js
+++ models/Recipient.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const { schema } = mongoose;
+const { Schema } = mongoose;
 
 const recipientSchema = new Schema({
   email: String,
-  responded: { type: boolean, default: false }
+  responded: { type: Boolean, default: false }
 });
 
 module.exports = recipientSchema;
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -12,10 +14,13 @@ module.exports = app => {
         title, //ES6 syntax when you have the key and value the same (title: title,)
         subject,
         body,
-        recipients: recipients.split(',').map(email => ({ email: email.trim() })), 
+        recipients: recipients.split(',').map(email => ({ email: email.trim() })),
         _user: req.user.id,
         dateSent: Date.now()
-
       });
+      //send an email here
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
+
   });
 };
