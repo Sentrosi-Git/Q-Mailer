e76bf1ae36a9aa1ab671886930ba3966cd0587ee
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-05-23 11:39:27 +1000

    sendgrid fixes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div>
       <SurveyList />
       <div className="container fixed-action-btn">
-        <Link to="/surveys/new" className="btn-floating btn-large blue">
+        <Link to="/surveys/new" className="btn-floating btn-large blue right">
           <i className="material-icons">add</i>
         </Link>
       </div>
